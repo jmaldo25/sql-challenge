5f8e445475dc5008dac05d28df6b476bf80c04ea
Josue Maldonado <joshmaldo@Josues-MacBook-Pro.local>
2021-05-22 17:10:40 -0500

    hire date analysis

--- employee_queries.sql
+++ employee_queries.sql
@@ -2,3 +2,14 @@
 
 --List the following details of each employee: employee number, last name, first name, sex, salary
 --Using inner join
+SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, employees.salary
+FROM employees
+JOIN salaries ON
+employees.emp_no = salaries.emp_no
+
+--List first name, last name, and hire date for employees who were hired in 1986
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31';
+
+--

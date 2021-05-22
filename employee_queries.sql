2dcc717a1ff705186ab1cc44cbfe21a7a8c2230b
Josue Maldonado <joshmaldo@Josues-MacBook-Pro.local>
2021-05-22 18:20:02 -0500

    added last name desc analysis

--- employee_queries.sql
+++ employee_queries.sql
@@ -53,3 +53,9 @@ employees.emp_no = dep_emp.emp_no
 JOIN departments ON
 dept_emp.dept_no = departments.dept_no
 WHERE departments.dept_name = 'Sales' OR departments.dept_name = 'Development';
+
+--In descending order, list the frequency count of employee last names, i.e., how many employees share each last name
+SELECT last_name, COUNT(last_name) AS "frequency_count"
+FROM employees
+GROUP BY last_name
+ORDER BY frequency_count DESC;

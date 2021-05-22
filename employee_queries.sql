d0617637571fa6b43167b937555f8b9e37fb9d7d
Josue Maldonado <joshmaldo@Josues-MacBook-Pro.local>
2021-05-22 18:17:16 -0500

    added sales and development analysis

--- employee_queries.sql
+++ employee_queries.sql
@@ -43,3 +43,13 @@ employees.emp_no = departments.dept_no
 JOIN departments ON
 dept_emp.dept_no = departments.dept_no
 WHERE departments.dept_name = 'Sales';
+
+--List all employees in the Sales and Development departments,
+--including their employee number, last name, first name, and department name
+SELECT employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM employees
+JOIN dept_emp ON
+employees.emp_no = dep_emp.emp_no
+JOIN departments ON
+dept_emp.dept_no = departments.dept_no
+WHERE departments.dept_name = 'Sales' OR departments.dept_name = 'Development';

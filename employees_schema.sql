6d7362441e6f40abd558fdc76e08207b4bfdacbd
Josue Maldonado <joshmaldo@Josues-MacBook-Pro.local>
2021-05-22 16:38:46 -0500

    added department managers table

--- employees_schema.sql
+++ employees_schema.sql
@@ -39,3 +39,10 @@ CREATE TABLE dept_emp(
 emp_no INT NOT NULL,
 dept_no VARCHAR(30) NOT NULL,
 FOREIGN KEY (emp_no) REFERENCES employees(emp_no));
+
+--Department managers table
+CREATE TABLE dept_manager(
+    dept_no VARCHAR(30) NOT NULL,
+    emp_no INT NOT NULL,
+    FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no));

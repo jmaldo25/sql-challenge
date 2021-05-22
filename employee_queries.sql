f25ec37244c656d900db631d48cdd723492072f1
Josue Maldonado <joshmaldo@Josues-MacBook-Pro.local>
2021-05-22 17:17:06 -0500

    analysis for dept managers

--- employee_queries.sql
+++ employee_queries.sql
@@ -12,4 +12,13 @@ SELECT first_name, last_name, hire_date
 FROM employees
 WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31';
 
+--List the manager of each department with the following information:
+--department number, department name, the manager's employee number,last name, first name
+SELECT departments.dept_no, departments.dept_name, dept_manager.emp_no, employees.last_name, employees.first_name
+FROM departments
+JOIN dept_manager ON
+dept_manager.dept_no = departments.dept_no
+JOIN employees ON
+employees.emp_no = dept_manager.emp_no;
+
 --

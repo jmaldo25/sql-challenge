a52facfaab02c575424d42d47f636c597d63387e
Josue Maldonado <joshmaldo@Josues-MacBook-Pro.local>
2021-05-22 18:01:48 -0500

    Sales employees analysis

--- employee_queries.sql
+++ employee_queries.sql
@@ -35,4 +35,11 @@ SELECT first name, last name, sex
 FROM employees
 WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
 
---
+--List all employees in the Sales department, including their employee number, last name, first name, and department name
+SELECT employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM employees
+JOIN dept_emp ON
+employees.emp_no = departments.dept_no
+JOIN departments ON
+dept_emp.dept_no = departments.dept_no
+WHERE departments.dept_name = 'Sales';

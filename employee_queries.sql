6fc1da432fe019c9662e673bcaf70945283ab722
Josue Maldonado <joshmaldo@Josues-MacBook-Pro.local>
2021-05-22 17:22:43 -0500

    added employee dept analysis

--- employee_queries.sql
+++ employee_queries.sql
@@ -21,4 +21,11 @@ dept_manager.dept_no = departments.dept_no
 JOIN employees ON
 employees.emp_no = dept_manager.emp_no;
 
---
+--List the department of each employee with the following information:
+--employee number, last name, first name, and department name
+SELECT employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM employees
+JOIN dept_emp ON
+employees.emp_no = dept_emp.emp_no
+JOIN departments ON
+dept_emp.dept_no = departments.dept_no;
